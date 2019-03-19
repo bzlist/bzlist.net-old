69ff471a1b1ced91e64784a8d6d86b4b00271459
The Noah <thenoahbz@gmail.com>
2019-03-19 16:14:54 -0500

    Fix formatting

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 
 import {Server} from "./server";
-import { Observable } from 'rxjs';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: "root"
