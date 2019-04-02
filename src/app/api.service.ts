5426427b15c98f7d1d3672845d59b11af49264f6
The Noah <thenoahbz@gmail.com>
2019-04-02 16:50:48 -0500

    Update to new DB location

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   providedIn: "root"
 })
 export class ApiService{
-  readonly API_ROOT_URL: string = "https://bzlist.ns01.biz/";
+  readonly API_ROOT_URL: string = "https://gist.githubusercontent.com/The-Noah/020e7550a55ae88c5fd9e856e7afed65/raw/3c58ce9d6a2dfa408d19cc814248951154d9522d/db.json";
   
   constructor(private http: HttpClient){}
 
