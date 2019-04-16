865589c6aa693cf570a0d39b7ab5b33abeda0a78
The Noah <thenoahbz@gmail.com>
2019-04-15 21:39:35 -0500

    Fix change detection and sorting

--- src/app/Servers/servers.component.ts
+++ src/app/Servers/servers.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, OnInit, ChangeDetectorRef, ChangeDetectionStrategy} from "@angular/core";
+import {Component, ViewChild, OnInit} from "@angular/core";
 import {MatTableDataSource, MatSort, MatDialog, MatPaginator, MatSnackBar, MatSnackBarRef, SimpleSnackBar} from "@angular/material";
 
 import {CookieService} from "ngx-cookie-service";
@@ -11,8 +11,7 @@ import {Time} from "../time";
 @Component({
   selector: "servers",
   templateUrl: "./servers.component.html",
-  styleUrls: ["./servers.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ["./servers.component.scss"]
 })
 export class ServersComponent implements OnInit{
   lastClientUpdate: string = "never";
@@ -55,15 +54,12 @@ export class ServersComponent implements OnInit{
   constructor(private apiService: ApiService,
               private dialog: MatDialog,
               private snackBar: MatSnackBar,
-              private cookieService: CookieService,
-              private changeRef: ChangeDetectorRef){
+              private cookieService: CookieService){
   }
 
   ngOnInit(): void{
-    this.changeRef.detach();
-    
     this.serverData = new MatTableDataSource<Server>();
-    setTimeout(() => this.serverData.sort = this.sort);
+    setTimeout(() => this.serverData.sort = this.sort, 1);
     this.serverData.paginator = this.paginator;
 
     this.serverData.filterPredicate = (data: Server, filters: string) => {
@@ -124,8 +120,6 @@ export class ServersComponent implements OnInit{
 
       this.updateTimestamps();
       this.openSnackBar("Refreshed");
-
-      this.changeRef.detectChanges();
     }, error => {
       this.openSnackBar("Error fetching data", undefined, 10000);
       console.error("Update servers error", error);
