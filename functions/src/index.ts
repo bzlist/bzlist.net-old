6586e0105b246ebedb618c93828f1c3e524e07cd
The Noah <thenoahbz@gmail.com>
2019-04-30 18:41:19 -0500

    Remove extra url logs from ssr function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,13 +4,10 @@ const universal = require(`${process.cwd()}/dist/server`).app;
 // export const ssr = functions.https.onRequest(universal);
 export const ssr = functions.https.onRequest((req, res) => {
   console.log(`Url: ${req.url}`);
-  console.log(`Path: ${req.path}`);
 
   if(!req.path){
     req.url = `/${req.url}`;
   }
-
-  console.log(`Final Url: ${req.url}`);
 
   return universal(req, res);
 });
