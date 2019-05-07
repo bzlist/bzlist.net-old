d9a1493b8222c728bec7e54956dbcf8e275f67ca
The Noah <thenoahbz@gmail.com>
2019-05-07 16:39:24 -0500

    Add time-ago.pipe auto update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 *Note: this is an outline and does not contain every change.*
 
+## Unreleased
+
+### Additions
+- time-ago pipe auto update
+
 ## HOTFIX: 0.3.21 (2019-5-7)
 
 - Fix component name `servers` (now `app-servers`) in home page.
--- src/app/pipes/time-ago.pipe.ts
+++ src/app/pipes/time-ago.pipe.ts
@@ -1,4 +1,4 @@
-import {Pipe, PipeTransform} from "@angular/core";
+import {Pipe, PipeTransform, OnDestroy, ChangeDetectorRef, NgZone} from "@angular/core";
 
 // time constants
 const MINUTE = 60;
@@ -9,31 +9,67 @@ const DAY = HOUR * 24;
   name: "timeAgo",
   pure: false
 })
-export class TimeAgoPipe implements PipeTransform{
-  transform(value: number, args?: any): string{
-    if(value < 0){
-      return "never";
-    }
+export class TimeAgoPipe implements PipeTransform, OnDestroy{
+  private timer: number;
 
+  constructor(private changeDectectorRef: ChangeDetectorRef,
+              private ngZone: NgZone){
+  }
+
+  transform(value: number): string{
     // convert from timestamp to time ago
     value = new Date().getTime() / 1000 - value;
+    const timeToUpdate = Number.isNaN(value) ? 1000 : this.getSecondsUntilUpdate(value) * 1000;
 
     let time = "just now";
 
+    if(value < 0){
+      time = "never";
+    }
+
     if(value >= DAY * 2){
       time = `${Math.floor(value / DAY)} days ago`;
-    }else if(value >= DAY){
+    }else if (value >= DAY){
       time = "a day ago";
-    }else if(value >= HOUR * 2){
+    }else if (value >= HOUR * 2){
       time = `${Math.floor(value / HOUR)} hours ago`;
-    }else if(value >= HOUR){
+    }else if (value >= HOUR) {
       time = "an hour ago";
-    }else if(value >= MINUTE * 2){
+    }else if (value >= MINUTE * 2){
       time = `${Math.floor(value / MINUTE)} minutes ago`;
-    }else if(value >= MINUTE){
+    }else if (value >= MINUTE){
       time = "a minute ago";
     }
 
+    console.log(`time-ago.pipe: ${time}`);
+
     return time;
+  }
+
+  ngOnDestroy(): void{
+    this.removeTimer();
+  }
+
+  private removeTimer(){
+		if(this.timer){
+			window.clearTimeout(this.timer);
+			this.timer = null;
+		}
+  }
+
+  private getSecondsUntilUpdate(seconds: number){
+    if(seconds < MINUTE){
+      // less than 1 min, update every 10 seconds
+      return 10;
+    }else if(seconds < HOUR){
+      // less than an hour, update every 30 seconds
+      return 30;
+    }else if(seconds < DAY){
+      // less then a day, update every 5 minutes
+      return MINUTE * 5;
+    }else{
+      // update every hour
+      return HOUR;
+    }
   }
 }
