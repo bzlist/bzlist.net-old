e28f38aff8bd3c75b2ec4e4dac73e168f0ea1fe9
The Noah <thenoahbz@gmail.com>
2019-07-07 21:21:28 -0500

    Remove extra console.log from SSR function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,6 @@ const universal = require(`${process.cwd()}/dist/server`).app;
 
 // export const ssr = functions.https.onRequest(universal);
 export const ssr = functions.https.onRequest((req, res) => {
-  console.log(`Url: ${req.url}`);
-
   if(!req.path){
     req.url = `/${req.url}`;
   }
