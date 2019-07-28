f01397281dec8dc248ad532f911281fb16d4e8da
The Noah <thenoahbz@gmail.com>
2019-07-28 15:23:41 -0500

    Rename project to bzlist.net

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("Welcome to bzlist-client!");
+    expect(page.getTitleText()).toEqual("Welcome to bzlist.net!");
   });
 
   afterEach(async () => {
