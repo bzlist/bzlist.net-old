e34088c32707aa814efedd3a3d500961238e6aac
The Noah <thenoahbz@gmail.com>
2019-08-06 17:48:58 -0500

    Make back button on server page actually go back

--- src/app/pages/server-page/server-page.component.ts
+++ src/app/pages/server-page/server-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, OnDestroy} from "@angular/core";
+import {Location} from "@angular/common";
 import {ActivatedRoute} from "@angular/router";
 
 import {Subscription} from "rxjs";
@@ -34,7 +35,8 @@ export class ServerPageComponent implements OnInit, OnDestroy{
   private teamSort: string;
   private teamSortOrder = 1;
 
-  constructor(private route: ActivatedRoute,
+  constructor(private location: Location,
+              private route: ActivatedRoute,
               private afs: AngularFirestore,
               private seo: SeoService){
   }
