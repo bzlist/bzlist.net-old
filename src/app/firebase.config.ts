b170a41a2750b8fd7816a40dc02f4abb5b187fb7
The Noah <thenoahbz@gmail.com>
2019-08-30 15:32:59 -0500

    Remove old Firebase stuff

--- src/app/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-export const firebase = {
-  apiKey: "AIzaSyBd24W4vVkfTXjWbDF2ItwUlXtHXK3fN_o",
-  authDomain: "bzlist-api.firebaseapp.com",
-  databaseURL: "https://bzlist-api.firebaseio.com",
-  projectId: "bzlist-api"
-};
