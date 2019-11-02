1b0ff2a9c0001ebed49d6bde54ebb4a34ed5a0d1
The Noah <thenoahbz@gmail.com>
2019-11-02 17:30:55 -0500

    Add SEO tags to privacy policy & terms of service

--- src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
+++ src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,10 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import {SeoService} from "@app/services";
 
 @Component({
   selector: "app-privacy-policy-page",
   templateUrl: "./privacy-policy-page.component.html"
 })
-export class PrivacyPolicyPageComponent{
-  constructor(){
+export class PrivacyPolicyPageComponent implements OnInit{
+  constructor(private seo: SeoService){
+  }
+
+  ngOnInit(){
+    this.seo.generateTags({
+      title: "Privacy Policy - BZList",
+      description: "BZList's privacy policy"
+    });
   }
 }
--- src/app/pages/terms-of-service-page/terms-of-service-page.component.ts
+++ src/app/pages/terms-of-service-page/terms-of-service-page.component.ts
@@ -1,10 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import {SeoService} from "@app/services";
 
 @Component({
   selector: "app-terms-of-service-page",
   templateUrl: "./terms-of-service-page.component.html"
 })
-export class TermsOfServicePageComponent{
-  constructor(){
+export class TermsOfServicePageComponent implements OnInit{
+  constructor(private seo: SeoService){
+  }
+
+  ngOnInit(){
+    this.seo.generateTags({
+      title: "Terms of Service - BZList",
+      description: "BZList's terms of service"
+    });
   }
 }
